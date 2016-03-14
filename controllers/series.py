7ac5ab92567c58a9fe22843088a0c9346953e783
kadowa <waeschle@ub.uni-heidelberg.de>
2016-03-14 14:39:17 +0100

    Alphabetical order of series, close #11

--- controllers/series.py
+++ controllers/series.py
@@ -4,10 +4,8 @@ Copyright (c) 2015 Heidelberg University Library
 Distributed under the GNU GPL v3. For full terms see the file
 LICENSE.md
 '''
-locale = 'de_DE'
-if session.forced_language == 'en':
-  locale = 'en_US'
 
+from ompdal import OMPDAL
 
 def saa_info():
 	return dict()
@@ -28,24 +26,31 @@ def cms_info():
         return dict()
 
 def index():
-  series = db((db.series.series_id==db.series_settings.series_id) & (db.series.press_id==myconf.take("omp.press_id")) & (db.series_settings.locale==locale)).select(db.series.path, db.series.image, db.series_settings.series_id, db.series_settings.setting_name, db.series_settings.setting_value, orderby= [db.series_settings.series_id, db.series_settings.setting_name] )
-  series_metadata =[]
-  types = ['title','subtitle','description']
-  prev_series = 0
-  for  row in series:
-    if row['series_settings']['series_id'] != prev_series:
-      metadata = {}
-      prev_series = row['series_settings']['series_id']
-      metadata['path'] = row['series']['path']
-      metadata['image'] = row['series']['image']
-      series_metadata.append(metadata)
-      
-    for t in types:
-      if row['series_settings']['setting_name']== t:
-        metadata[t] =   row['series_settings']['setting_value']
-    
-        
-  series_ids = db(db.series.press_id==myconf.take("omp.press_id")).select(db.series.series_id)
-  if len(series_ids) == 0 :
+  if session.forced_language == 'de':
+    locale = 'de_DE'
+  if session.forced_language == 'en':
+     locale = 'en_US'
+
+  ompdal = OMPDAL(db, myconf)
+  
+  series = ompdal.getSeries()
+  if len(series) == 0:
     raise HTTP(200, "'invalid': no series in this press")
+
+  setting_types = ['title','subtitle','description']
+  order = []
+  for s in series:
+    series_info = dict()
+    series_info['path'] = s.path
+    series_info['image'] = s.image
+    settings = ompdal.getLocalizedSeriesSettings(s.series_id, locale)
+    if not settings:
+      settings = ompdal.getSeriesSettings(s.series_id)
+    for st in settings:
+      if st.setting_name in setting_types:
+	series_info[st.setting_name] = st.setting_value
+    order.append(series_info)
+
+  order.sort(key=lambda s: s.get('title', 'z'))
+
   return locals()
--- modules/ompdal.py
+++ modules/ompdal.py
@@ -30,3 +30,12 @@ class OMPDAL:
 		except:
 			return []
 		return self.db((self.db.authors.submission_id==submission_id) & (self.db.authors.user_group_id==chapter_author_group_id)).select(self.db.authors.first_name, self.db.authors.middle_name, self.db.authors.last_name, orderby=self.db.authors.seq)
+
+	def getSeries(self):
+		return self.db(self.db.series.press_id==self.conf.take("omp.press_id")).select(self.db.series.series_id, self.db.series.path, self.db.series.image)
+
+	def getLocalizedSeriesSettings(self, series_id, locale):
+		return self.db((self.db.series_settings.series_id==series_id) & (self.db.series_settings.locale==locale)).select(self.db.series_settings.series_id, self.db.series_settings.locale, self.db.series_settings.setting_name, self.db.series_settings.setting_value)
+
+        def getSeriesSettings(self, series_id):
+        	return self.db(self.db.series_settings.series_id==series_id).select(self.db.series_settings.series_id, self.db.series_settings.locale, self.db.series_settings.setting_name, self.db.series_settings.setting_value)
