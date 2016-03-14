928b5fde2b9a6b6a4756da9d1303c525759eb013
kadowa <waeschle@ub.uni-heidelberg.de>
2016-03-14 13:20:27 +0100

    Fix NameError if author_id or editor_id is missing

--- modules/ompdal.py
+++ modules/ompdal.py
@@ -18,7 +18,7 @@ class OMPDAL:
 		try:
 			editor_group_id = self.conf.take('omp.editor_id')
 		except:
-			pass#editor_group_id = self.db.user_group_settings(self.db.user_group_settings.setting_value=="Volume editor").user_group_id
+			return []
 		return self.db((self.db.authors.submission_id==submission_id) & (self.db.authors.user_group_id==editor_group_id)).select(self.db.authors.first_name, self.db.authors.middle_name, self.db.authors.last_name, orderby=self.db.authors.seq)
 
 	def getChapterAuthors(self, submission_id):
@@ -28,5 +28,5 @@ class OMPDAL:
 		try:
 			chapter_author_group_id = self.conf.take('omp.author_id')
 		except:
-			pass#chapter_author_group_id = self.db.user_group_settings(self.db.user_group_settings.setting_value=="Chapter author").user_group_id
+			return []
 		return self.db((self.db.authors.submission_id==submission_id) & (self.db.authors.user_group_id==chapter_author_group_id)).select(self.db.authors.first_name, self.db.authors.middle_name, self.db.authors.last_name, orderby=self.db.authors.seq)
