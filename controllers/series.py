a985887ad3d0f8f32fce83ec141ea7a8793f6186
Dulip Withanage <dulip.withanage@gmail.com>
2019-09-03 13:45:53 +0200

    https://gitlab.ub.uni-heidelberg.de/wit/verlag-portale/issues/297

--- controllers/series.py
+++ controllers/series.py
@@ -7,6 +7,7 @@ LICENSE.md
 
 from ompdal import OMPDAL, OMPSettings, OMPItem
 from os.path import exists
+
 ompdal = OMPDAL(db, myconf)
 cur_locale = __import__("locale")
 
@@ -18,34 +19,38 @@ else:
     lang = ''
 cur_locale.setlocale(cur_locale.LC_ALL, 'de_DE.utf-8')
 
+
 def info():
     if request.args == []:
-        redirect( URL('home', 'index'))
+        redirect(URL('home', 'index'))
     series_path = request.args[0]
-    
-    if exists(request.folder+'views/series/'+series_path+"_info.html"):
-        content = "series/"+series_path+"_info.html"
+
+    if exists(request.folder + 'views/series/' + series_path + "_info.html"):
+        content = "series/" + series_path + "_info.html"
     else:
-        redirect( URL('home', 'index'))
-        
+        redirect(URL('home', 'index'))
+
     return locals()
 
+
 def index():
-
-        
-
-    
     # Load press info from config
     press = ompdal.getPress(myconf.take('omp.press_id'))
     if not press:
         redirect(URL('home', 'index'))
-    
+
     all_series = []
     for row in ompdal.getSeriesByPress(press.press_id):
-        all_series.append(OMPItem(row, OMPSettings(ompdal.getSeriesSettings(row.series_id)), 
-            {'series_editors': [OMPItem(u, OMPSettings(ompdal.getUserSettings(u.user_id))) 
-                                for u in ompdal.getSeriesEditors(press.press_id, row.series_id)]}))
-        
+        all_series.append(OMPItem(row, OMPSettings(ompdal.getSeriesSettings(row.series_id)),
+                                  {
+                                      'series_editors'   : [OMPItem(u, OMPSettings(ompdal.getUserSettings(u.user_id)))
+                                                            for u in
+                                                            ompdal.getSeriesEditors(press.press_id, row.series_id)],
+                                      'series_categories': [
+                                          OMPItem(u, OMPSettings(ompdal.getCategorySettings(u.category_id)))
+                                          for u in ompdal.getCategoriesBySeries(row.series_id)]
+                                      }))
+
     all_series.sort(key=lambda s: cur_locale.strxfrm(s.settings.getLocalizedValue('title', lang)))
-    
+
     return locals()
